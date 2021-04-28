6817f82a01354b2c4d1ecf613ef9b8c6f7c52f42
fanxuankai <fan_xuankai@126.com>
2021-04-28 19:47:48 +0800

    RedisTemplate json 序列化包含类信息;增加 RedisUtils 工具类.

--- fxk-boot-starter-redis/src/main/java/com/fanxuankai/boot/redis/RedisAutoConfiguration.java
+++ fxk-boot-starter-redis/src/main/java/com/fanxuankai/boot/redis/RedisAutoConfiguration.java
@@ -12,28 +12,32 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
 /**
+ * RedisTemplate 自动装配, 如果装配成功会自动激活 RedisUtils
+ *
  * @author fanxuankai
  */
 @Configuration
+@EnableRedisUtils
+@ConditionalOnMissingBean(name = "redisTemplate")
 public class RedisAutoConfiguration {
-
     @Bean
-    @ConditionalOnMissingBean(name = "redisTemplate")
     public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
-        Jackson2JsonRedisSerializer<?> json = new Jackson2JsonRedisSerializer<>(Object.class);
+        Jackson2JsonRedisSerializer<?> jsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
         ObjectMapper om = new ObjectMapper();
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        om.deactivateDefaultTyping();
-        json.setObjectMapper(om);
+        // JSON 字符串包含类型信息
+        om.activateDefaultTyping(om.getPolymorphicTypeValidator(), ObjectMapper.DefaultTyping.NON_FINAL);
+        jsonRedisSerializer.setObjectMapper(om);
         RedisSerializer<String> string = RedisSerializer.string();
+        // key 和 hashKey 使用字符串序列化
         redisTemplate.setKeySerializer(string);
         redisTemplate.setHashKeySerializer(string);
-        redisTemplate.setValueSerializer(json);
-        redisTemplate.setHashValueSerializer(json);
+        // value 和 hashValue 使用 JSON 序列化
+        redisTemplate.setValueSerializer(jsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(jsonRedisSerializer);
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
     }
-
 }

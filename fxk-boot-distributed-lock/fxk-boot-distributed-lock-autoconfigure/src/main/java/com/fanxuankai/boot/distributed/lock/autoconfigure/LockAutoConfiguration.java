a9e6694d2cf4019fcabe1699ec5389cc6d8a3ee1
fanxuankai <fan_xuankai@126.com>
2021-05-14 10:41:42 +0800

    lock 注解失效问题

--- fxk-boot-distributed-lock/fxk-boot-distributed-lock-autoconfigure/src/main/java/com/fanxuankai/boot/distributed/lock/autoconfigure/LockAutoConfiguration.java
+++ fxk-boot-distributed-lock/fxk-boot-distributed-lock-autoconfigure/src/main/java/com/fanxuankai/boot/distributed/lock/autoconfigure/LockAutoConfiguration.java
@@ -1,7 +1,6 @@
 package com.fanxuankai.boot.distributed.lock.autoconfigure;
 
 import com.fanxuankai.boot.distributed.lock.DistributedLocker;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 
@@ -9,12 +8,9 @@ import org.springframework.context.annotation.Bean;
  * @author fanxuankai
  */
 public class LockAutoConfiguration {
-
     @Bean
     @ConditionalOnMissingBean
-    @ConditionalOnBean({DistributedLocker.class})
     public LockPointcutAdvisor lockPointcutAdvisor(DistributedLocker distributedLocker) {
         return new LockPointcutAdvisor(new LockMethodInterceptor(distributedLocker));
     }
-
 }

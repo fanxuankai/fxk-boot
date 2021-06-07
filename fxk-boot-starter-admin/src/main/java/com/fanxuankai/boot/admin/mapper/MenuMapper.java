10dddc3fc9dc4bb224b4ad49ea187e50b38101c4
fanxuankai <fan_xuankai@126.com>
2021-06-07 11:14:59 +0800

    starter admin 代码优化

--- fxk-boot-starter-admin/src/main/java/com/fanxuankai/boot/admin/mapper/MenuMapper.java
+++ fxk-boot-starter-admin/src/main/java/com/fanxuankai/boot/admin/mapper/MenuMapper.java
@@ -2,6 +2,9 @@ package com.fanxuankai.boot.admin.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.fanxuankai.boot.admin.model.Menu;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * 菜单 Mapper 接口
@@ -9,4 +12,17 @@ import com.fanxuankai.boot.admin.model.Menu;
  * @author fanxuankai
  */
 public interface MenuMapper extends BaseMapper<Menu> {
+    /**
+     * 获取用户的权限
+     *
+     * @param userId 用户id
+     * @return List
+     */
+    @Select("select m.permission from menu m " +
+            "LEFT JOIN role_menu rp on rp.menu_id = m.id " +
+            "LEFT JOIN role r on r.id = rp.role_id " +
+            "LEFT JOIN user_role ur on ur.role_id = r.id " +
+            "LEFT JOIN user u on u.id = ur.user_id " +
+            "WHERE u.id = #{userId}")
+    List<String> listPermissions(Long userId);
 }
